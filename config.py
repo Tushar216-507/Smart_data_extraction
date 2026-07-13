897d35016689705c38b65f9e3e13b70438bfd182
Tushar <tushar.kadam@auxilo.com>
2026-07-13 17:35:20 +0530

    Started Working on Azure OPENAI pdf extraction

--- config.py
+++ config.py
@@ -4,8 +4,15 @@ load_dotenv()
 
 class Config:
     GROQ_API_KEY = os.getenv('GROQ_API_KEY')
+    GROQ_MODEL = os.getenv('GROQ_MODEL')
+
     OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
     OPENAI_MODEL = os.getenv('OPENAI_MODEL')
-    GROQ_MODEL = os.getenv('GROQ_MODEL')
+
     NVIDIA_API_KEY = os.getenv('NVIDIA_API_KEY')
     NVIDIA_MODEL = os.getenv('NVIDIA_MODEL')
+
+    AZURE_IMG_ENDPOINT=os.getenv('AZURE_IMG_ENDPOINT')
+    AZURE_PDF_ENDPOINT=os.getenv('AZURE_PDF_ENDPOINT')
+    AZURE_IMG_KEY=os.getenv('AZURE_IMG_KEY')
+    AZURE_PDF_KEY=os.getenv('AZURE_PDF_KEY')
